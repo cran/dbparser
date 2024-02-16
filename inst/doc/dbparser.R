363d4f91e662d1384d946c78376ceb0a98e9fabc
Mohammed Ali <moh_fcis@yahoo.com>
2024-02-16 17:10:04 +0000

    version 2.0.2

--- inst/doc/dbparser.R
+++ inst/doc/dbparser.R
@@ -6,74 +6,82 @@ knitr::opts_chunk$set(
   out.width = "100%"
 )
 
-## ----eval=FALSE---------------------------------------------------------------
-#  ## load dbparser package
-#  library(dbparser)
-#  library(dplyr)
-#  library(ggplot2)
-#  library(XML)
-#  
-#  ## parse data from XML and save it to memory
-#  dvobj <- parseDrugBank(db_path            = "C:\drugbank.xml",
-#                         drug_options       = drug_node_options(),
-#                         parse_salts        = TRUE,
-#                         parse_products     = TRUE,
-#                         references_options = references_node_options(),
-#                         cett_options       = cett_nodes_options())
-#  
-#  ## load drugs data
-#  drugs <- dvobj$drugs$general_information
-#  
-#  ## load drug groups data
-#  drug_groups <- dvobj$drugs$groups
-#  
-#  ## load drug targets actions data
-#  drug_targets_actions <- dvobj$cett$targets$actions
+## ----eval=T-------------------------------------------------------------------
+## load dbparser package
+suppressPackageStartupMessages({
+  library(tidyr)
+  library(dplyr)
+  library(canvasXpress)
+  library(tibble)
+  library(dbparser)
+})
 
-## ----eval=FALSE---------------------------------------------------------------
-#  ## view proportions of the different drug types (biotech vs. small molecule)
-#  drugs %>%
-#      select(type) %>%
-#      ggplot(aes(x = type, fill = type)) +
-#      geom_bar() +
-#      guides(fill = FALSE)     ## removes legend for the bar colors
 
-## ----eval=FALSE---------------------------------------------------------------
-#  ## view proportions of the different drug types for each drug group
-#  drugs %>%
-#      full_join(drug_groups, by = c('primary_key' = 'drugbank_id')) %>%
-#      select(type, group) %>%
-#      ggplot(aes(x = group, fill = type)) +
-#      geom_bar() +
-#      theme(legend.position = 'bottom') +
-#      labs(x = 'Drug Group',
-#           y = 'Quantity',
-#           title = "Drug Type Distribution per Drug Group",
-#           caption = "created by ggplot") +
-#      coord_flip()
+## load drugs data
+drugs <- readRDS(system.file("drugs.RDS", package = "dbparser"))
 
-## ----eval=FALSE---------------------------------------------------------------
-#  ## get counts of the different target actions in the data
-#  targetActionCounts <-
-#      drug_targets_actions %>%
-#      group_by(action) %>%
-#      summarise(count = n()) %>%
-#      arrange(desc(count))
-#  
-#  ## get bar chart of the 10 most occurring target actions in the data
-#  p <-
-#      ggplot(targetActionCounts[1:10,],
-#             aes(x = reorder(action,count), y = count, fill = letters[1:10])) +
-#      geom_bar(stat = 'identity') +
-#      labs(fill = 'action',
-#           x = 'Target Action',
-#           y = 'Quantity',
-#           title = 'Target Actions Distribution',
-#           subtitle = 'Distribution of Target Actions in the Data',
-#           caption = 'created by ggplot') +
-#      guides(fill = FALSE) +    ## removes legend for the bar colors
-#      coord_flip()              ## switches the X and Y axes
-#  
-#  ## display plot
-#  p
+## load drug groups data
+drug_groups <- readRDS(system.file("drug_groups.RDS", package = "dbparser"))
+
+## load drug targets actions data
+drug_targets_actions <- readRDS(system.file("targets_actions.RDS", package = "dbparser"))
+
+## ----eval=T-------------------------------------------------------------------
+## view proportions of the different drug types (biotech vs. small molecule)
+type_stat <- drugs %>% 
+  select(type) %>% 
+  group_by(type) %>% 
+  summarise(count = n()) %>% 
+  column_to_rownames("type")
+
+canvasXpress(
+  data             = type_stat,
+  graphOrientation = "vertical",
+  graphType        = "Bar",
+  showSampleNames  = FALSE,
+  title            ="Drugs Type Distribution",
+  xAxisTitle       = "Count"
+)
+
+## ----eval=T-------------------------------------------------------------------
+## view proportions of the different drug types for each drug group
+type_stat <- drugs %>% 
+  full_join(drug_groups, by = c("drugbank_id")) %>% 
+  select(type, group) %>% 
+  group_by(type, group) %>% 
+  summarise(count = n()) %>% 
+  pivot_wider(names_from = group, values_from = count) %>% 
+  column_to_rownames("type")
+
+canvasXpress(
+  data           = type_stat,
+  graphType      = "Stacked",
+  legendColumns  = 2,
+  legendPosition = "bottom",
+  title          ="Drug Type Distribution per Drug Group",
+  xAxisTitle     = "Quantity",
+  xAxis2Show     = TRUE,
+  xAxisShow      = FALSE,
+  smpTitle      = "Drug Group")
+
+## ----eval=T-------------------------------------------------------------------
+## get counts of the different target actions in the data
+targetActionCounts <- 
+    drug_targets_actions %>% 
+    group_by(action) %>% 
+    summarise(count = n()) %>% 
+    arrange(desc(count)) %>% 
+    top_n(10) %>% 
+    column_to_rownames("action")
+
+## get bar chart of the 10 most occurring target actions in the data
+canvasXpress(
+  data            = targetActionCounts,
+  graphType       = "Bar",
+  legendColumns   = 2,
+  legendPosition  = "bottom",
+  title           = "Target Actions Distribution",
+  showSampleNames = FALSE,
+  xAxis2Show      = TRUE,
+  xAxisShow       = FALSE)
 
